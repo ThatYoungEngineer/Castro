a8e81c3fd0d9279ebb0743d5ae9af9d9af17615a
faris3211 <muhammad.faris3211@gmail.com>
2024-01-11 02:32:14 +0500

    hello

--- src/store/store.js
+++ src/store/store.js
@@ -7,8 +7,8 @@ const store = configureStore({
         cart: cartReducer
     },
 
-    middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(cartMiddleware),
+    // middleware: (getDefaultMiddleware) =>
+    // getDefaultMiddleware().concat(cartMiddleware),
 
     devTools: true
 })
